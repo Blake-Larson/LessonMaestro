dd69fc41a951e6461b2231753b4355d3b8078a03
Blake Larson <Blarson224@gmail.com>
2023-03-25 14:12:33 -0700

    removing lessons

--- src/server/api/routers/lessonRouter.ts
+++ src/server/api/routers/lessonRouter.ts
@@ -3,16 +3,16 @@ import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const lessonRouter = createTRPCRouter({
-  getLessons: protectedProcedure.query(async ({ ctx }) => {
-    const lessons = await ctx.prisma.lesson.findMany({
-      select: {
-        archived: true,
-        attendance: true,
-        date: true,
-        id: true,
-        payment: true,
-      },
-    });
-    return lessons;
-  }),
+  // getLessons: protectedProcedure.query(async ({ ctx }) => {
+  //   const lessons = await ctx.prisma.lesson.findMany({
+  //     select: {
+  //       archived: true,
+  //       attendance: true,
+  //       date: true,
+  //       id: true,
+  //       payment: true,
+  //     },
+  //   });
+  //   return lessons;
+  // }),
 });

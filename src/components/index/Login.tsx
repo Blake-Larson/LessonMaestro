415a9cb541c158d5cb9bea2727566dfd90663ce5
Blake Larson <Blarson224@gmail.com>
2023-03-27 13:36:45 -0700

    document fix 2

--- src/components/index/Login.tsx
+++ src/components/index/Login.tsx
@@ -1,10 +1,8 @@
 import React from "react";
-// import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 
 const Login = () => {
-  // const { data: sessionData } = useSession();
   const { isLoaded: isSignedIn } = useUser();
   const { user } = useUser();
 
@@ -19,12 +17,6 @@ const Login = () => {
             Dashboard
           </Link>
         )}
-        {/* <button
-          className="btn-primary btn"
-          onClick={sessionData ? () => void signOut() : () => void signIn()}
-        >
-          {sessionData ? "Sign out" : "Sign in"}
-        </button> */}
         <div className="border-slate-400 flex border-b p-4">
           {/* {!isSignedIn && ( */}
           <div className="flex justify-center">

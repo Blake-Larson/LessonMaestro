a181c813151bf107e3dcd813b42aa960794e70d2
Blake Larson <Blarson224@gmail.com>
2023-03-28 09:34:57 -0700

    back to Image

--- src/components/index/Card.tsx
+++ src/components/index/Card.tsx
@@ -12,14 +12,13 @@ function Card({ src, alt, title, text }: Props) {
   return (
     <div className="card bg-base-100 shadow-xl md:w-96">
       <figure className="relative h-72 w-full">
-        {/* <Image
+        <Image
           src={src}
           fill
           sizes="(max-width: 768px) 100vw, 33vw"
           className="object-cover"
           alt={alt}
-        /> */}
-        <img src={src} alt={alt} />
+        />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{title}</h2>

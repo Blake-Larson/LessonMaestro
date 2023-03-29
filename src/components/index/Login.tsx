c1a8da31c2bc515e286236e9650106874ee1fa9c
Blake Larson <Blarson224@gmail.com>
2023-03-29 13:48:38 -0700

    changed sign in button

--- src/components/index/Login.tsx
+++ src/components/index/Login.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import Link from "next/link";
-import { SignInButton, useUser } from "@clerk/nextjs";
+import { useUser } from "@clerk/nextjs";
 
 const Login = () => {
-  const { user, isSignedIn } = useUser();
+  const { isSignedIn } = useUser();
 
   return (
     <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-white text-center text-2xl">
-        {isSignedIn && <span>Hey {user?.firstName}, welcome back!</span>}
-      </p>
       <div className="flex flex-col gap-3">
         {isSignedIn && (
           <Link href={"/dashboard"} className="btn-secondary btn">
@@ -18,7 +15,7 @@ const Login = () => {
         )}
         {!isSignedIn && (
           <div className="btn-secondary btn">
-            <SignInButton />
+            <Link href={"/sign-in"}>Sign In</Link>
           </div>
         )}
       </div>
